e755eca0a27c78d02d6e13f107cc82d9705b5702
Francis Y. Yan <fyy@cs.stanford.edu>
2018-05-02 02:10:24 +0000

    Create Pantheon app and set up MySQL

---
+++ pantheon/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PantheonConfig(AppConfig):
+    name = 'pantheon'
--- pantheon_website/settings.py
+++ pantheon_website/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['pantheon.stanford.edu']
 # Application definition
 
 INSTALLED_APPS = [
+    'pantheon.apps.PantheonConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'pantheon_website.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'pantheon',
+        'USER': 'pantheon',
+        'PASSWORD': os.environ['PANTHEON_DB_PWD'],
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
