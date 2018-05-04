07684fba9de352d204e03269eeda57b0a6f89d19
Francis Y. Yan <fyy@cs.stanford.edu>
2018-05-04 13:53:51 -0700

    models.py: add choices

--- pantheon/models.py
+++ pantheon/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class Fileset(models.Model):
+    # experiment type
     NODE_EXPT = 'node'
     CLOUD_EXPT = 'cloud'
     EMU_EXPT = 'emu'
@@ -19,7 +20,15 @@ class Fileset(models.Model):
     graph2 = models.URLField()
     time = models.IntegerField()
     runs = models.IntegerField()
-    scenario = models.CharField(max_length=64)
+
+    # flow scenario
+    ONE_FLOW = '1_flow'
+    THREE_FLOWS = '3_flows'
+    SCENARIO_CHOICES = (
+        (ONE_FLOW, '1 flow'),
+        (THREE_FLOWS, '3 flows'),
+    )
+    scenario = models.CharField(choices=SCENARIO_CHOICES, max_length=16)
 
 
 class NodeExpt(Fileset):
@@ -38,6 +47,9 @@ class NodeExpt(Fileset):
 
         return '{} to {}, {}'.format(src, dst, self.link)
 
+    def description(self):
+        return self.__str__()
+
 
 class CloudExpt(Fileset):
     src = models.CharField(max_length=64)
@@ -46,14 +58,42 @@ class CloudExpt(Fileset):
     def __str__(self):
         return '{} to {}'.format(self.src, self.dst)
 
+    def description(self):
+        return self.__str__()
+
 
 class EmuExpt(Fileset):
-    emu_scenario = models.IntegerField()
-    emu_cmd = models.CharField(max_length=255)
+    # emulation scenario
+    EMU_SCENARIO_CHOICES = (
+        (1, 'Calibrated emulator (Nepal to AWS India)'),
+        (2, 'Calibrated emulator (Mexico cellular to AWS California)'),
+        (3, 'Calibrated emulator (AWS Brazil to Colombia cellular)'),
+        (4, 'Calibrated emulator (India to AWS India)'),
+        (5, 'Calibrated emulator (AWS Korea to China)'),
+        (6, 'Calibrated emulator (AWS California to Mexico)'),
+        (7, 'Token-bucket based policer (bandwidth 12mbps, RTT 20ms)'),
+        (8, 'Token-bucket based policer (bandwidth 60mbps, RTT 20ms)'),
+        (9, 'Token-bucket based policer (bandwidth 108mbps, RTT 20ms)'),
+        (10, 'Token-bucket based policer (bandwidth 12mbps, RTT 100ms)'),
+        (11, 'Token-bucket based policer (bandwidth 60mbps, RTT 100ms)'),
+        (12, 'Token-bucket based policer (bandwidth 108mbps, RTT 100ms)'),
+        (13, 'Severe ACK aggregation (1 ACK every 100ms)'),
+        (14, 'Severe ACK aggregation (10 ACKs every 200ms)'),
+        (15, 'Bottleneck buffer = BDP/10'),
+        (16, 'Bottleneck buffer = BDP/3'),
+        (17, 'Bottleneck buffer = BDP/2'),
+        (18, 'Bottleneck buffer = BDP'),
+    )
+    emu_scenario = models.IntegerField(choices=EMU_SCENARIO_CHOICES)
+
+    emu_cmd = models.CharField(max_length=512)
     emu_desc = models.CharField(max_length=512)
 
     def __str__(self):
         return 'scenario {}, {}'.format(self.emu_scenario, self.emu_desc)
+
+    def description(self):
+        return self.__str__()
 
 
 class Perf(models.Model):
